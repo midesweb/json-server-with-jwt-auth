5c2e976906496d220f4285802fa0b6ed030d463c
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2017-11-01 12:23:08 +0100

    Token expries 10min

--- security/jwt.js
+++ security/jwt.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const secret = 'DesarrolloWeb.com/EscuelaIT'
 
 /** cifra el usuario durante un margen de tiempo */
-exports.tokenGeneration = (user) => jwt.sign(user, secret, { expiresIn: 60 })
+exports.tokenGeneration = (user) => jwt.sign(user, secret, { expiresIn: '10m' })
 
 /** verifica al usuario a partir del token  */
 exports.verify = (token) => {
